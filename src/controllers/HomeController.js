ed0578106b1f72712e7723c1c271bd35731f683e
LeVanHung34554 <hung091124858@gmail.com>
2023-05-04 14:31:30 +0700

    update

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,4 +1,6 @@
-let getHomePage =(req,res)=>{
+require('dotenv').config();
+import request from "request";
+let getHomePage = (req, res) => {
     return res.render('homepage.ejs')
 };
 let getWebhook = (req, res) => {
@@ -9,9 +11,7 @@ let getWebhook = (req, res) => {
     // Parse the query params
     let mode = req.query['hub.mode'];
     let token = req.query['hub.verify_token'];
-    let challenge = req.query['hub.challenge'];
-
-    // Checks if a token and mode is in the query string of the request
+    let challenge = req.query['hub.challenge']; // Checks if a token and mode is in the query string of the request
     if (mode && token) {
 
         // Checks the mode and token sent is correct
@@ -36,7 +36,7 @@ let postWebhook = (req, res) => {
     if (body.object === 'page') {
 
         // Iterate over each entry - there may be multiple if batched
-        body.entry.forEach(function(entry) {
+        body.entry.forEach(function (entry) {
 
             // Gets the body of the webhook event
             let webhook_event = entry.messaging[0];
@@ -66,8 +66,92 @@ let postWebhook = (req, res) => {
     }
 
 };
-module.exports={
-    getHomePage:getHomePage,
-    postWebhook:postWebhook,
-    getWebhook:getWebhook,
+let handleMessage = (sender_psid, received_message) => {
+    let response;
+
+    // Checks if the message contains text
+    if (received_message.text) {
+        // Create the payload for a basic text message, which
+        // will be added to the body of our request to the Send API
+        response = {
+            "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+        }
+    } else if (received_message.attachments) {
+        // Get the URL of the message attachment
+        let attachment_url = received_message.attachments[0].payload.url;
+        response = {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "generic",
+                    "elements": [{
+                        "title": "Is this the right picture?",
+                        "subtitle": "Tap a button to answer.",
+                        "image_url": attachment_url,
+                        "buttons": [
+                            {
+                                "type": "postback",
+                                "title": "Yes!",
+                                "payload": "yes",
+                            },
+                            {
+                                "type": "postback",
+                                "title": "No!",
+                                "payload": "no",
+                            }
+                        ],
+                    }]
+                }
+            }
+        }
+    }
+    // Send the response message
+    callSendAPI(sender_psid, response);
+};
+
+// Handles messaging_postbacks events
+let handlePostback = (sender_psid, received_postback) => {
+    let response;
+
+    // Get the payload for the postback
+    let payload = received_postback.payload;
+
+    // Set the response based on the postback payload
+    if (payload === 'yes') {
+        response = { "text": "Thanks!" }
+    } else if (payload === 'no') {
+        response = { "text": "Oops, try sending another image." }
+    }
+    // Send the message to acknowledge the postback
+    callSendAPI(sender_psid, response);
+};
+
+// Sends response messages via the Send API
+let callSendAPI = (sender_psid, response) => {
+    // Construct the message body
+    let request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": response
+    };
+    // Send the HTTP request to the Messenger Platform
+    request({
+        "uri": "https://graph.facebook.com/v6.0/me/messages",
+        "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('message sent!')
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
+};
+module.exports = {
+    getHomePage: getHomePage,
+    postWebhook: postWebhook,
+    getWebhook: getWebhook,
+
 }
