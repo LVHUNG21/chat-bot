4bc9e4179a9d35938d1e5ffc25a593205a9aa845
LeVanHung34554 <hung091124858@gmail.com>
2023-05-05 10:52:24 +0700

    update

--- src/services/chatbotService.js
+++ src/services/chatbotService.js
@@ -25,30 +25,33 @@ let callSendAPI = (sender_psid, response) => {
     })
         ;
 }
-let getUserName = async (sender_psid) => {
-    let username='';
-     await request({
-        "uri": `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}>`,
-        "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
-        "method": "GET",
-    }, (err, res, body) => { 
-        console.log(body);
-        if (!err) {
-            let response =JSON.parse(res);
-            // "first_name":"le",
-            // "last_name":"hung"
-            username=`${response.first_name} ${response.last_name}`
-        } else {
-            console.error("Unable to send message:" + err);
-        }
+let getUserName = (sender_psid) => {
+    return new Promise((resolve, reject) => {
+        request({
+            "uri": `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}>`,
+            "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+            "method": "GET",
+        }, (err, res, body) => {
+            console.log(body);
+            if (!err) {
+                body = JSON.parse(body);
+                // "first_name":"le",
+                // "last_name":"hung"
+                let username = `${body.first_name} ${body.last_name}`
+                resolve(username);
+            } else {
+                console.error("Unable to send message:" + err);
+                reject(err)
+            }
+        })
+            
     })
-     ;
-     return username;
+
 }
 let handleGetStarted = (sender_psid) => {
     return new Promise(async (resolve, reject) => {
         try {
-            let username=await getUserName(sender_psid);
+            let username = await getUserName(sender_psid);
             let response = { "text": `OK.XIN CHAO ${username}  DEN VOI NHA HANG` }
             await callSendAPI(sender_psid, response)
             resolve('done');
