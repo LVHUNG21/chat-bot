aef3f806340f9a657101921229df3a44ba2b1332
LeVanHung34554 <hung091124858@gmail.com>
2023-05-05 10:46:42 +0700

    update

--- src/services/chatbotService.js
+++ src/services/chatbotService.js
@@ -1,41 +1,63 @@
 require('dotenv').config();
 import request from "request";
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
-let callSendAPI=(sender_psid,response)=>{
+let callSendAPI = (sender_psid, response) => {
     //sender_psid laf nguoi nhan tin nhan 
-  // Construct the message body
-  let request_body = {
-    "recipient": {
-        "id": sender_psid
-    },
-    "message": response
-};
-// Send the HTTP request to the Messenger Platform
-request({
-    "uri": "https://graph.facebook.com/v9.0/me/messages",
-    "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
-    "method": "POST",
-    "json": request_body
-}, (err, res, body) => {
-    if (!err) {
-        console.log('message sent!')
-    } else {
-        console.error("Unable to send message:" + err);
-    }
-});
+    // Construct the message body
+    let request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": response
+    };
+    // Send the HTTP request to the Messenger Platform
+    request({
+        "uri": "https://graph.facebook.com/v9.0/me/messages",
+        "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('message sent!')
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    })
+        ;
 }
-let handleGetStarted= (sender_psid)=>{
-    return new Promise(async (resolve,reject)=>{
+let getUserName = async (sender_psid) => {
+    let username='';
+     await request({
+        "uri": `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}>`,
+        "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+        "method": "GET",
+    }, (err, res, body) => { 
+        console.log(body);
+        if (!err) {
+            let response =JSON.parse(res);
+            // "first_name":"le",
+            // "last_name":"hung"
+            username=`${response.first_name} ${response.last_name}`
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    })
+     ;
+     return username;
+}
+let handleGetStarted = (sender_psid) => {
+    return new Promise(async (resolve, reject) => {
         try {
-             let response = { "text": "OK.XIN CHAO DEN VOI NHA HANG" }
-             await callSendAPI(sender_psid,response)
-             resolve('done');
+            let username=await getUserName(sender_psid);
+            let response = { "text": `OK.XIN CHAO ${username}  DEN VOI NHA HANG` }
+            await callSendAPI(sender_psid, response)
+            resolve('done');
 
-        }catch(e){
+        } catch (e) {
             reject(e);
         }
-    }) 
+    })
 }
-module.exports={
-    handleGetStarted:handleGetStarted
+module.exports = {
+    handleGetStarted: handleGetStarted
 }
