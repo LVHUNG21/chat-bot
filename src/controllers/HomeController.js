b79235aea5f9817d98a050a4845a1e079d8bd1da
LeVanHung34554 <hung091124858@gmail.com>
2023-05-05 10:08:26 +0700

    update

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -122,6 +122,8 @@ let handlePostback = (sender_psid, received_postback) => {
         response = { "text": "Thanks!" }
     } else if (payload === 'no') {
         response = { "text": "Oops, try sending another image." }
+    }else if(payload==="GET_STARTED"){
+        response={"text":"OK.XIN CHAO DEN VOI NHA HANG"}
     }
     // Send the message to acknowledge the postback
     callSendAPI(sender_psid, response);
