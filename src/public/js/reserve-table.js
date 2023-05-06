538ff468290bcf505b23701470629abb75b61ff1
LeVanHung34554 <hung091124858@gmail.com>
2023-05-06 14:16:56 +0700

    update

--- src/public/js/reserve-table.js
+++ src/public/js/reserve-table.js
@@ -11,7 +11,7 @@ window.extAsyncInit = function () {
     //phai doi fb load xong moi co dc  mess extensions
     // the Messenger Extensions JS SDK is done loading 
 
-    MessengerExtensions.getContext('1554138315079604', // get Context:get psid of user open webview
+    MessengerExtensions.getContext('753933095237424', // get Context:get psid of user open webview
         function success(thread_context) {
               console.log('handleClickbutton');
             // success
