bb90c982ff5075cc7cfc8b26c7c3674d784b6979
LeVanHung34554 <hung091124858@gmail.com>
2023-05-07 19:19:51 +0700

    u

--- src/public/js/reserve-table.js
+++ src/public/js/reserve-table.js
@@ -53,7 +53,7 @@ function validateInputFields() {
 
 
 function handleClickButtonReserveTable() {
-    $("#btnReserveTable").on("click", function (e) {
+    $("#btnRes").on("click", function (e) {
         let check = validateInputFields(); //return true or false
 
         let data = {
